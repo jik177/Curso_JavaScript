60ea22cfee7ecc1cd6ae871c7399c4c14a14131f
jik177 <60604868+jik177@users.noreply.github.com>
2023-12-12 13:34:51 +0100

    Add files via upload

---
+++ 2_Inicial_JS/ejercicio_4_funciones/js/script6.js
@@ -0,0 +1,12 @@
+// 1º declaracion de variables locales
+
+function cta() {
+    var usuario= document.getElementById('user').value;
+    /* Importante reduccion de la recogida de la variable,
+    es decir, el script va adaptandose y muta a menores caracteres. */
+    var password= pw.value;
+
+}
+
+
+
