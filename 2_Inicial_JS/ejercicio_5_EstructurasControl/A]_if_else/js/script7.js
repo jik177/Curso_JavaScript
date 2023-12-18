7be69c3dbc243f5f5eeac35f0a886b11b6eba699
jik177 <60604868+jik177@users.noreply.github.com>
2023-12-18 13:35:32 +0100

    Add files via upload

--- 2_Inicial_JS/ejercicio_5_EstructurasControl/A]_if_else/js/script7.js
+++ 2_Inicial_JS/ejercicio_5_EstructurasControl/A]_if_else/js/script7.js
@@ -17,22 +17,33 @@ function dilexia() {
         /* creamos otra propiedad muy util, que 
         ayuda a trabajar no dentro del html,
         sino desde css.*/
-        document.getElementById('resultado').style.border='3px solid black';
+        document.getElementById('resultado').style.border = '3px solid black';
+        /* y creamos el valor para provocar el cambio. */
+        estiloDilexia = true;
     } else {
-        document.getElementById('resultado').style.border=''
+        document.getElementById('resultado').style.border = '';
+        estiloDilexia = false;
     }
 }
-function altonico() {
+function daltonico() {
     if (estiloDaltonico == false) {
-
+        document.getElementById('resultado').style.backgroundColor = 'grey';
+        document.getElementById('resultado').style.color = 'black';
+        estiloDaltonico = true;
     } else {
-
+        document.getElementById('resultado').style.backgroundColor = '#2196f3';
+        document.getElementById('resultado').style.color = ' white';
+        estiloDaltonico = false;
     }
 }
 function epilepsia() {
     if (estiloEpilepsia == false) {
-
+        /* no utilizamos la propiedad 'style' sino .className, 
+        que esta llamando a un style */
+        document.getElementById('resultado').className='sombra';
+        estiloEpilepsia = true;
     } else {
-
+        document.getElementById('resultado').className='';
+        estiloEpilepsia = false;
     }
 }
---
+++ 2_Inicial_JS/ejercicio_5_EstructurasControl/B]_Switch_case/js/script2.js
@@ -0,0 +1,36 @@
+function menuDia() {
+    var menu = new Date();
+    var dia = menu.getDay();
+    //variable reesidual o de apoyo
+    var menu = '';
+    //estructura de control
+    switch (dia) {
+        case 0:
+
+            break;
+        case 0:
+
+            break;
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        default:
+            break;
+    }
+
+}
