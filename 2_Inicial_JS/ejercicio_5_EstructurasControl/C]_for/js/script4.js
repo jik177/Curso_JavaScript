9344a41aa39bf0556c6209cfee8288a18db01a82
jik177 <60604868+jik177@users.noreply.github.com>
2023-12-21 11:11:31 +0100

    Add files via upload

---
+++ 2_Inicial_JS/ejercicio_5_EstructurasControl/C]_for/js/script4.js
@@ -0,0 +1,17 @@
+/* Mostrar nuestro array */
+
+var provincias = ['.....', 'Valencia', 'Madrid', 'Sevilla', 'Málaga'];
+
+/* ahora necesitamos guardar todas las provincias que hay 
+en una variable para ello usamos el el 'for' */
+
+for (var p = 0; p < provincias.length; p++) {
+    var provinciasOpcion = new Option(provincias[p]);
+    document.getElementById('mostrandoProvoncias').options[p] = provinciasOpcion
+
+}
+
+function Selection() {
+    document.getElementById('mostrandoProvoncias').options[document.getElementById('mostrandoProvoncias').selectedIndex].text = provinciasOpcion;
+    /* '.selectIndex' es pla propiedad de seleccion de un elemento o elementos de html llamandolo com objeto */
+}
